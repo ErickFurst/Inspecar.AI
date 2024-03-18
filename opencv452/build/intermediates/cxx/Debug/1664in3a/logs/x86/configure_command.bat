30d9ddf8d310135fa8945934d5ff23714f8ff08a
ErickFurst <31510579+ErickFurst@users.noreply.github.com>
2024-03-18 17:11:31 -0300

    Initial commit

---
+++ opencv452/build/intermediates/cxx/Debug/1664in3a/logs/x86/configure_command.bat
@@ -0,0 +1,19 @@
+@echo off
+"C:\\Users\\Erick\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\Erick\\AndroidStudioProjects\\InspecarAI\\opencv452\\libcxx_helper" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=21" ^
+  "-DANDROID_PLATFORM=android-21" ^
+  "-DANDROID_ABI=x86" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86" ^
+  "-DANDROID_NDK=C:\\Users\\Erick\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\Erick\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\Erick\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\Erick\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\Erick\\AndroidStudioProjects\\InspecarAI\\opencv452\\build\\intermediates\\cxx\\Debug\\1664in3a\\obj\\x86" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\Erick\\AndroidStudioProjects\\InspecarAI\\opencv452\\build\\intermediates\\cxx\\Debug\\1664in3a\\obj\\x86" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BC:\\Users\\Erick\\AndroidStudioProjects\\InspecarAI\\opencv452\\.cxx\\Debug\\1664in3a\\x86" ^
+  -GNinja ^
+  "-DANDROID_STL=c++_shared"
